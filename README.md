f1ed19e653ab646ef58b1b3d82d080dd158350be
Todd Croak-Falen <toddcf@gmail.com>
2020-06-22 19:58:22 -0700

    Created Exercises object. Filled in data except for the Bonus exercises.

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# foundation
+# Foundation
---
+++ app.js
@@ -0,0 +1,83 @@
+const exercises = {
+  founder: {
+    title: 'The Founder',
+    level: 'Basic',
+    transition: 5000,
+    pose1: 15000,
+    pose2: 15000,
+    pose3: 15000,
+    pose4: 6000,
+    pose5: 15000,
+    pose6: 15000,
+    pose7: 20000
+  },
+  foundationSquat: {
+    title: 'Foundation Squat',
+    level: 'Moderate',
+    transition: 3000,
+    pose1: 50000
+  },
+  goodMorning: {
+    title: 'Good Morning',
+    level: 'Intense',
+    transition: 3000,
+    pose1: 42000
+  },
+  windmill: {
+    title: 'Windmill',
+    level: 'Intense',
+    transition: 3000,
+    pose1: 15000,
+    pose2: 30000,
+    pse3: 30000
+  }
+  woodpecker: {
+    title: 'Woodpecker',
+    level: 'Moderate',
+    transition: 5000,
+    pose1: 5000,
+    pose2: 5000,
+    pose3: 20000,
+    pose4: 3000,
+    pose5: 15000
+  },
+  backExt: {
+    title: 'Back Extension',
+    level: 'Basic',
+    transition: 10000,
+    pose1: 30000
+  },
+  foundationPlank: {
+    title: 'Foundation Plank',
+    level: 'Intense',
+    transition: 5000,
+    pose1: 10000,
+    pose2: 30000,
+    pose3: 10000
+  }
+  addAssBackExt: {
+    title: 'Adductor-Assisted Back Extension',
+    level: 'Basic',
+    transition: 5000,
+    pose1: 20000
+  },
+  childPoseKneeling: {
+    title: 'Child\'s Pose / Kneeling Founder',
+    level: 'Basic',
+    transition: 3000
+    pose1: 30000,
+    pose2: 15000,
+    pose3: 10000
+  },
+  lungeStretch: {
+    title: 'Lunge Stretch',
+    level: 'Basic',
+    transition: 10000,
+    pose1: 20000,
+    shift: 5000,
+    pose2: 20000
+  },
+  crossover: {
+    
+  }
+}
